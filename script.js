d983fba1c6451248ec9aedb14845c58fd1cf9ce7
kadoflorence <kadoflorence@gmail.com>
2024-10-22 15:51:13 -0300

    Mudanças, banner dinamico, nav, adição da sessão serviços

---
+++ script.js
@@ -0,0 +1,18 @@
+let slides = document.querySelectorAll('.banner-slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Mudar a cada 3 segundos
